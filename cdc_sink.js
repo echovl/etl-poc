b4577e42b7f2bc6c7d66256b2a96847ee0a3abf4
Alonso Villegas <alonsovillegas816@gmail.com>
2021-09-27 14:30:07 -0500

    catch pub sub errors

--- cdc_sink.js
+++ cdc_sink.js
@@ -13,31 +13,35 @@ const bigquery = new BigQuery()
 const subscription = pubsub.topic(TOPIC_NAME).subscription(SUBSCRIPTION_NAME)
 
 subscription.on("message", async (message) => {
-    const doc = JSON.parse(message.data.toString())
+    try {
+        const doc = JSON.parse(message.data.toString())
 
-    const rows = [
-        {
-            id: doc._id,
-            number: doc.orderNumber,
-            sell_status: doc.orderSellStatus,
-            serve_status: doc.orderServeStatus,
-            type: doc.orderType,
-            tax_amount: doc.taxAmount * 100,
-            discount_amount: doc.discountAmount * 100,
-            delivery_amount: doc.deliveryAmount * 100,
-            subtotal_amount: doc.subTotalAmount * 100,
-            total_amount: doc.totalAmount * 100,
-            store_id: doc.storeId,
-            merchant_id: doc.merchantId,
-            created_at: doc.createdAt,
-            updated_at: doc.updatedAt,
-        },
-    ]
+        const rows = [
+            {
+                id: doc._id,
+                number: doc.orderNumber,
+                sell_status: doc.orderSellStatus,
+                serve_status: doc.orderServeStatus,
+                type: doc.orderType,
+                tax_amount: doc.taxAmount * 100,
+                discount_amount: doc.discountAmount * 100,
+                delivery_amount: doc.deliveryAmount * 100,
+                subtotal_amount: doc.subTotalAmount * 100,
+                total_amount: doc.totalAmount * 100,
+                store_id: doc.storeId,
+                merchant_id: doc.merchantId,
+                created_at: doc.createdAt,
+                updated_at: doc.updatedAt,
+            },
+        ]
 
-    console.log(`order ${doc._id} received`)
+        console.log(`order ${doc._id} received`)
 
-    await bigquery.dataset(DATASET_ID).table(TABLE_ID).insert(rows)
-    message.ack()
+        await bigquery.dataset(DATASET_ID).table(TABLE_ID).insert(rows)
+        message.ack()
 
-    console.log(`order ${doc._id} inserted to bigquery`)
+        console.log(`order ${doc._id} inserted to bigquery`)
+    } catch (err) {
+        console.log("error: ", JSON.stringify(err))
+    }
 })
